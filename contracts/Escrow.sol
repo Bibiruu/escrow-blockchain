0845fa9cd7f5ddde14e5d1e92b9dbe568a40bbc8
Maria <so.mariagloria@gmail.com>
2024-08-19 03:20:04 +0300

    smart contract features for escrow

---
+++ contracts/Escrow.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+contract Escrow {
+    address public payer;
+    address payable public payee;
+    address public lawyer;
+    uint256 public amount;
+    
+    constructor(
+        address _payer, 
+        address payable _payee, 
+        uint256 _amount) {
+            payer = _payer;
+            payee =_payee;
+            lawyer = msg.sender;
+            amount = _amount;
+        }
+
+    function deposit() payable public {
+        require(msg.sender == payer, "Sender must be the payer");
+        //having the correct amount in contract
+        require(address(this).balance <= amount);
+    }
+
+    function release() public {
+        require(address(this).balance == amount, "Cannot release funds before full amount is sent");
+        require(msg.sender == lawyer, "Only lawyer can release funds");
+        payee.transfer(amount);
+    }
+
+    function balanceOf() view public returns(uint256) {
+        return address(this).balance;
+    }
+}
---
+++ truffle-config.js
@@ -0,0 +1,40 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
+
+// Exponential backoff formula: 2^attempt * 1000 (milliseconds)
+const exponentialBackoff = (attempt) => {
+  return Math.pow(2, attempt) * 1000;
+};
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+    },
+
+    sepolia: {
+      provider: () => new HDWalletProvider(
+        process.env.MNEMONIC,
+        `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHMY_ENDPOINT}`
+      ),
+      network_id: 11155111,
+      gas: 6000000,               // Gas limit
+      gasPrice: 20000000000,       // 20 Gwei
+      confirmations: 1,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+      networkCheckTimeout: 10000,  // Increase if necessary
+      clientConfig: {
+        maxAttempts: 5,
+        interval: exponentialBackoff,
+      }
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.8.21",
+    }
+  },
+};
